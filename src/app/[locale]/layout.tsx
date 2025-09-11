95e29920d805e43a3ad85670dd857bf249fbdb04
bachacode <85910843+bachacode@users.noreply.github.com>
2025-09-11 19:20:59 -0400

    fix: properly typed params in LocaleLayout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -20,7 +20,7 @@ export default async function LocaleLayout({
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }>) {
   const { locale } = await params;
 
