8f897f5565b603b24d6a3413087e4510f397f8c0
bachacode <85910843+bachacode@users.noreply.github.com>
2025-09-18 20:06:03 -0400

    feat: removed json-ld schemas from LocaleLayout and added schemas to homepage

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,12 +3,10 @@ import { setRequestLocale } from "next-intl/server";
 import { notFound } from "next/navigation";
 import MainHeader from "@/components/layout/MainHeader";
 import MainFooter from "@/components/layout/MainFooter";
-import { hasLocale, NextIntlClientProvider, useTranslations } from "next-intl";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
 import ProgressBarProvider from "@/components/layout/ProgressBarProvider";
 import { Analytics } from "@vercel/analytics/next";
 import { Roboto } from "next/font/google";
-import Script from "next/script";
-import Head from "next/head";
 
 const roboto = Roboto({ weight: "500", subsets: ["latin"], display: "swap" });
 
@@ -25,8 +23,6 @@ export default async function LocaleLayout({
 }>) {
   const { locale } = await params;
 
-  const t = useTranslations("metadata");
-
   // Ensure that the incoming `locale` is valid
   if (!hasLocale(routing.locales, locale)) {
     notFound();
@@ -37,62 +33,6 @@ export default async function LocaleLayout({
 
   return (
     <html lang={locale}>
-      <Head>
-        <Script
-          id="organization-schema"
-          type="application/ld+json"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "Organization",
-              "@id": "https://bachacode.com/#organization",
-              name: "Bachacode Developers",
-              url: "https://bachacode.com",
-              logo: {
-                "@type": "ImageObject",
-                "@id": "https://bachacode.com/#logo",
-                inLanguage: locale,
-                height: 95,
-                width: 384,
-                contentUrl: "https://bachacode.com/images/bachacode.png",
-                url: "https://bachacode.com/images/bachacode.png",
-                caption: "Bachacode Developers",
-              },
-              image: { "@id": "https://bachacode.com/#logo" },
-              sameAs: [
-                "https://www.linkedin.com/company/bachacode-developers",
-                "https://github.com/bachacode",
-              ],
-              contactPoint: {
-                "@type": "ContactPoint",
-                telephone: "+58 412 116 3349",
-                email: "support@bachacode.com",
-                contactType: "customer service",
-                areaServed: "worldwide",
-                availableLanguage: ["Spanish", "English"],
-              },
-            }),
-          }}
-        />
-        <Script
-          id="website-schema"
-          type="application/ld+json"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              "@context": "https://schema.org",
-              "@type": "WebSite",
-              "@id": "https://bachacode.com/#website",
-              name: "Bachacode Developers",
-              url: "https://bachacode.com",
-              description: t("website.description"),
-              publisher: { "@id": "https://bachacode.com/#organization" },
-              inLanguage: locale,
-            }),
-          }}
-        />
-      </Head>
       <body className={`${roboto.className} bg-background/10`}>
         <NextIntlClientProvider>
           <ProgressBarProvider>
