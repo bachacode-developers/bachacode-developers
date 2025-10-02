9dd8e3502750e253bec47a6c03c30018001cdb94
Cristhian Flores <85910843+cristhianflo@users.noreply.github.com>
2025-10-02 18:41:03 -0400

    feat: created terms and conditions page
    Created a terms and conditions page and started with localization of
    identity of owner to english and spanish

---
+++ src/app/[locale]/legal/terms/page.tsx
@@ -0,0 +1,39 @@
+import { StructuredData } from "@/components/layout/StructuredData";
+import { useTranslations } from "next-intl";
+
+export default function Terms() {
+  const t = useTranslations("legal.terms");
+  return (
+    <StructuredData schemas={[]}>
+      <main className="container px-12">
+        <h1 className="py-6 text-4xl font-bold">{t("page.title")}</h1>
+        <section>
+          <h2 className="py-3 text-2xl font-semibold">
+            {t("page.identity.title")}
+          </h2>
+          <p>
+            {t.rich("page.identity.body", {
+              email: (chunks) => (
+                <a
+                  className="hover:text-primary font-bold underline transition-colors"
+                  href={`mailto:${chunks}`}
+                >
+                  {chunks}
+                </a>
+              ),
+              website: (chunks) => (
+                <a
+                  className="hover:text-primary font-bold underline transition-colors"
+                  href={`${chunks}`}
+                >
+                  {chunks}
+                </a>
+              ),
+              strong: (chunks) => <strong>{chunks}</strong>,
+            })}
+          </p>
+        </section>
+      </main>
+    </StructuredData>
+  );
+}
